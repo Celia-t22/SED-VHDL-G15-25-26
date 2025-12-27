668a30e43718ba617b0327953d9c5bb739396f3a
Carlotapg03 <carlota.pinedo03@gmail.com>
2025-12-27 23:11:40 +0100

    Añadido el top y los constraints + .tlc

---
+++ src/Top_Ascensor.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 27.12.2025 23:09:49
+-- Design Name: 
+-- Module Name: Top_Ascensor - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Top_Ascensor is
+--  Port ( );
+end Top_Ascensor;
+
+architecture Behavioral of Top_Ascensor is
+
+begin
+
+
+end Behavioral;
